c87bd98e929aa81e338c173428b4e961c9d3cd2a
coonseo <coonseo.322@gmail.com>
2023-03-26 20:57:21 +0600

    Methods show and create post

--- OurWIKI/post/__init__.py
+++ OurWIKI/post/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, request, redirect, url_for, g
+
+from OurWIKI.database import get_database
 
 post_blueprint = Blueprint('post',
                            __name__,
@@ -9,9 +11,30 @@ post_blueprint = Blueprint('post',
 
 @post_blueprint.route('/')
 def main():
-    return render_template('post/main.html')
+    database = get_database()
+    posts = database.execute(
+        'SELECT * FROM post ORDER BY created'
+    ).fetchall()
+    return render_template('post/main.html', posts=posts)
 
 
-@post_blueprint.route('/create')
+@post_blueprint.route('/create', methods=('GET', 'POST'))
 def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        error = None
+
+        if not title:
+            error = 'Title is required'
+
+        if error is None:
+            database = get_database()
+            database.execute(
+                "INSERT INTO post (title, description, author_id) VALUES (?, ?, ?)",
+                (title, description, g.user['id'])
+            )
+            database.commit()
+            return redirect(url_for('post.main'))
+
     return render_template('post/create_post.html')
